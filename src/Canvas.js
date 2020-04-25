9c795da4863a251d19945597cf7e6bbbb059270a
Mateus Soares <mateussoaress1997@gmail.com>
2020-04-25 00:16:38 -0300

    Optimizing fillGap method

--- src/Canvas.js
+++ src/Canvas.js
@@ -120,39 +120,29 @@ Canvas.prototype.fillGap = function(previousBlock, nextBlock)
         previousBlockX = parseInt(previousBlock[0]);
         previousBlockY = parseInt(previousBlock[1]);
 
-        var blockX = previousBlockX;
+        var newBlockX = previousBlockX;
 
         if(previousBlockX - nextBlockX > 1){
-            blockX = previousBlockX - 1;
+            newBlockX = previousBlockX - 1;
         }
 
         if(previousBlockX - nextBlockX < -1){
-            blockX = previousBlockX + 1;
+            newBlockX = previousBlockX + 1;
         }
 
-        var blockY = previousBlockY;
+        var newBlockY = previousBlockY;
 
         if(previousBlockY - nextBlockY > 1){
-            blockY = previousBlockY - 1;
+            newBlockY = previousBlockY - 1;
         }
 
         if(previousBlockY - nextBlockY < -1){
-            blockY = previousBlockY + 1;
+            newBlockY = previousBlockY + 1;
         }
 
-        newBlockToDraw = [blockX, blockY];
-
+        newBlockToDraw = [newBlockX,newBlockY];
+        this.drawBlock(newBlockX, newBlockY);
         blocksToDraw.push(newBlockToDraw);
-    }
-
-    for (let index = 0; index < blocksToDraw.length; index++) {
-
-        var targetX = blocksToDraw[index][0];
-        var targetY = blocksToDraw[index][1];
-
-        if(targetX > -1 && targetY > -1){
-            this.drawBlock(targetX, targetY);
-        }
     }
 
 }
